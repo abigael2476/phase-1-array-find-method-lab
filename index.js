26362dac61d608b873a34b0767cf85e4b30170b5
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-09-13 11:44:26 +0300

    added if

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+// index.js
+
+function superbowlWin(record) {
+  const winRecord = record.find((game) => game.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
+
+// Export the function so tests can use it
+module.exports = superbowlWin;
